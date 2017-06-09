5b02b51d7223bc14677034cdef6b66560a5755ac
Chinmay Wyawahare <chinmaynw@gmail.com>
2017-06-09 15:45:34 +0530

    Initial commit

---
+++ ViewModels/HelpViewModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using Nevala.Models;
+
+namespace Nevala.ViewModels
+{
+    public class HelpViewModel : ObservableObject
+    {
+        public ICommand HelpCommand { get; }
+
+        public HelpViewModel()
+        {
+            HelpCommand = new RelayCommand(null);
+        }
+
+        private void DisplayAbout()
+        {
+            //we will open help dialog
+        }
+    }
+}
